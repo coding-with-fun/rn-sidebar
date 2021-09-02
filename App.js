ea8f7be782e3b339b72c3b3ce5efa136fb816fb6
Harsh Patel <harsh.coderc@gmail.com>
2021-09-02 14:21:45 +0530

    Test Two

--- App.js
+++ App.js
@@ -1,27 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import "react-native-gesture-handler";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import "react-native-gesture-handler";
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
             <Drawer.Navigator>
-                <Drawer.Screen name="Feed" component={Feed} />
-                <Drawer.Screen name="Article" component={Article} />
+                <Drawer.Screen name="Home" component={Home} />
+                <Drawer.Screen
+                    name="StackedScreens"
+                    component={StackedScreens}
+                />
             </Drawer.Navigator>
         </NavigationContainer>
     );
 }
 
-export function Feed() {
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Home</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const screenOptionStyle = {
+    headerStyle: {
+        backgroundColor: "#9AC4F8",
+    },
+    headerTintColor: "white",
+    headerBackTitle: "Back",
+};
+
+export function StackedScreens() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Feed"
+                component={Feed}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Article"
+                component={Article}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+export function Feed({ navigation }) {
     return (
         <View style={styles.container}>
             <Text>Feed</Text>
+            <Button
+                title="Go to About Screen"
+                onPress={() => navigation.navigate("Article")}
+            />
             <StatusBar style="auto" />
         </View>
     );
